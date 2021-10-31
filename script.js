e06947abe30dbde31a1fd01a15cbbf1d4c5501ca
Rolwane Borges <rolwane@hotmail.com>
2021-10-31 17:32:06 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const p = document.querySelector('#carta-gerada');
 
+const classes = [
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
+
+function random(min, max) {
+  return Math.floor(Math.random() * ((max + 1) - min) + min);
+}
+
 function generateLetter() {
-  const letters = document.querySelector('#carta-texto').value.split(' ');
-  p.innerText = '';
-  let cont = 0;
-  letters.forEach((element) => {
-    if (element === '') {
-      cont += 1;
-    }
-  });
-  if (cont === letters.length) {
+  const text = document.querySelector('#carta-texto').value;
+  if (text.match(/[a-z]|[0-9]/gi)) {
+    const letters = text.split(' ');
+    p.innerText = '';
+    letters.forEach((letter) => {
+      const span = document.createElement('span');
+      const nClasses = random(1, 4);
+      for (let i = 0; i < nClasses; i += 1) {
+        const categoria = classes[random(0, 3)];
+        const classe = categoria[random(0, categoria.length - 1)];
+        span.classList.add(classe);
+      }
+      span.innerText = letter;
+      p.appendChild(span);
+    });
+  } else {
     p.innerText = 'Por favor, digite o conteúdo da carta.';
-    return;
   }
-  letters.forEach((letter) => {
-    const span = document.createElement('span');
-    span.innerText = letter;
-    p.appendChild(span);
-  });
 }
 
 document.querySelector('#criar-carta').addEventListener('click', generateLetter);
