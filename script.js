c8cbd03915ee6f0c89177aaf840ed50b8b972321
Rolwane Borges <rolwane@hotmail.com>
2021-10-31 16:26:23 -0300

    Requisito 05

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const p = document.querySelector('#carta-gerada');
 function generateLetter() {
   const letters = document.querySelector('#carta-texto').value.split(' ');
   p.innerText = '';
+  let cont = 0;
+  letters.forEach((element) => {
+    if (element === '') {
+      cont += 1;
+    }
+  });
+  if (cont === letters.length) {
+    p.innerText = 'Por favor, digite o conteúdo da carta.';
+    return;
+  }
   letters.forEach((letter) => {
     const span = document.createElement('span');
     span.innerText = letter;
